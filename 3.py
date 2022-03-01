18a0569e6a33925d36a9f176ebb9b0bc96f025c3
Aditya_Sai <2004sai@gmail.com>
2022-03-01 22:04:28 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,14 @@
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    factors.sort(reverse=True)
+    return factors[0]
+print(prime_factors(600851475143))
